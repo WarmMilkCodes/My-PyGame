fae8e41cf48b05177726969222d1706861a53a19
WarmMilkCodes <mrguybanks@gmail.com>
2022-06-01 18:53:14 -0500

    Add player class

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from email.mime import image
 import pygame
 from gameObject import GameObject
+from player import Player
 
 class Game:
     def __init__(self):
@@ -18,13 +19,21 @@ class Game:
         # Treasure
         self.treasure = GameObject(375, 50, 50, 50, 'assets/treasure.png')
 
+        # Clock
         self.clock = pygame.time.Clock()
     
+        # Player
+        self.player = Player(375, 700, 50, 50, 'assets/player.png', 10)
+
 
     def draw_objects(self):
         self.game_window.fill(self.black_color)        
+        
         self.game_window.blit(self.background.image, (self.background.x, self.background.y))
         self.game_window.blit(self.treasure.image, (self.treasure.x, self.treasure.y))
+        self.game_window.blit(self.player.image, (self.player.x, self.player.y))
+        
+        
         pygame.display.update()
 
     def run_game_loop(self):
